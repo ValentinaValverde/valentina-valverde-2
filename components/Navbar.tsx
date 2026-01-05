f794c214d209a341eec0a205ea7f5f75a7c576a5
Valentina Valverde <143435977+ValentinaValverde@users.noreply.github.com>
2026-01-05 17:22:56 -0500

    phase 2 navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,47 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function Navbar() {
+  const pathname = usePathname();
+
+  console.log(pathname);
+
   return (
     <div className="fixed top-0 mt-4 z-100 w-full flex justify-center items-center">
       <div className="bg-[var(--foreground)] py-4 px-8 rounded-full space-x-8">
-        <Link href="/" className="text-[var(--background)]">
+        <Link
+          href="/"
+          className={
+            pathname === '/'
+              ? 'text-[var(--background)]'
+              : 'text-[#9CAFB7] hover:text-[var(--background)] transition duration-300'
+          }
+        >
           Home
         </Link>
 
-        <a href="/projects" className="text-[var(--background)]">
+        <a
+          href="/projects"
+          className={
+            pathname === '/projects'
+              ? 'text-[var(--background)]'
+              : 'text-[#9CAFB7] hover:text-[var(--background)] transition duration-300'
+          }
+        >
           Projects
+        </a>
+
+        <a
+          href="/archives"
+          className={
+            pathname === '/projects'
+              ? 'text-[var(--background)]'
+              : 'text-[#9CAFB7] hover:text-[var(--background)] transition duration-300'
+          }
+        >
+          Archives
         </a>
       </div>
     </div>
