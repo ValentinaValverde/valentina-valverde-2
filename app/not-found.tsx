5dda9c556b0412ab8640005d1402f506f58ac648
Valentina Valverde <143435977+ValentinaValverde@users.noreply.github.com>
2026-01-05 17:25:37 -0500

    404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="h-[100vh] flex flex-col justify-center items-center gap-4">
+      <h1 className="text-4xl">404</h1>
+      <p>womp womp lil bro... or whatever the kids say these days. </p>
+    </div>
+  );
+}
