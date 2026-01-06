6292b42d7938ecfb368a7558fc217a3d5f14e77a
Valentina Valverde <143435977+ValentinaValverde@users.noreply.github.com>
2026-01-06 09:25:34 -0500

    getting started with projects route with page + header

---
+++ app/projects/page.tsx
@@ -0,0 +1,9 @@
+import ProjectsHeader from '@/components/projects/ProjectsHeader';
+
+export default function Projects() {
+  return (
+    <div className="space-y-24">
+      <ProjectsHeader />
+    </div>
+  );
+}
---
+++ components/projects/ProjectsHeader.tsx
@@ -0,0 +1,8 @@
+export default function ProjectsHeader() {
+  return (
+    <div className="h-[80vh] py-8 gap-4 flex flex-col justify-center items-center">
+      <p className="text-4xl !font-mono cursor-default">Project Directory</p>
+      <p className="opacity-50">Lorem ipsum dolor sit amet. </p>
+    </div>
+  );
+}
