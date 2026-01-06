3e4a3d987a0c850245503b96f9be8ab02a0e8a7f
Valentina Valverde <143435977+ValentinaValverde@users.noreply.github.com>
2026-01-06 09:18:16 -0500

    heart icon as a component so i can change the color on hover

--- components/About.tsx
+++ components/About.tsx
@@ -3,6 +3,27 @@
 import { useState } from 'react';
 import TimeItem from './TimeItem';
 
+export function HeartIcon({
+  size = '24',
+  className = '',
+}: {
+  size?: string;
+  className?: string;
+}) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 256 256"
+      fill="currentColor"
+      className={className}
+    >
+      <path d="M240,98a57.63,57.63,0,0,1-17,41L133.7,229.62a8,8,0,0,1-11.4,0L33,139a58,58,0,0,1,82-82.1L128,69.05l13.09-12.19A58,58,0,0,1,240,98Z" />
+    </svg>
+  );
+}
+
 export default function About() {
   const [isHovering, setIsHovered] = useState(false);
   const onMouseEnter = () => setIsHovered(true);
@@ -31,7 +52,15 @@ export default function About() {
         </div>
 
         <div className="flex items-center gap-2">
-          <img src="/icons/heart.svg" className="w-6 h-6 heartbeat" />
+          {/* <img
+            src="/icons/heart.svg"
+            className="w-6 h-6 heartbeat hover:text-white"
+          /> */}
+
+          <HeartIcon
+            size="24"
+            className="heartbeat text-black hover:text-[#CF3800] transition-colors"
+          />
 
           <p className="text-xl !font-mono cursor-default">
             Family & Sandwiches
