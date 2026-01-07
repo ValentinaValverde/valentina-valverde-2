18ebc4a420a2819b2ca686eb91ec002fce55f6bc
Valentina Valverde <143435977+ValentinaValverde@users.noreply.github.com>
2026-01-07 10:07:17 -0500

    add contacts section as footer of projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,11 +1,13 @@
 import ProjectsHeader from '@/components/projects/ProjectsHeader';
 import ProjectsSection from '@/components/projects/ProjectsSection';
+import Contact from '@/components/Contact';
 
 export default function Projects() {
   return (
     <div className="space-y-24">
       <ProjectsHeader />
       <ProjectsSection />
+      <Contact />
     </div>
   );
 }
