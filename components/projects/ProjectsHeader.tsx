72214840189eb36a9012e2a22c22a25b5a718289
Valentina Valverde <143435977+ValentinaValverde@users.noreply.github.com>
2026-01-07 22:29:38 -0500

    update description text on projects page

--- components/projects/ProjectsHeader.tsx
+++ components/projects/ProjectsHeader.tsx
@@ -2,7 +2,10 @@ export default function ProjectsHeader() {
   return (
     <div className="h-[80vh] py-8 gap-4 flex flex-col justify-center items-center">
       <p className="text-4xl !font-mono cursor-default">Project Directory</p>
-      <p className="opacity-50">Lorem ipsum dolor sit amet. </p>
+      <p className="opacity-50 text-center w-2/3 md:w-[45%]">
+        I&apos;`ve had the pleasure of helping SaaS startups reenvision their
+        platform in a way that truly speaks to the user.
+      </p>
     </div>
   );
 }
